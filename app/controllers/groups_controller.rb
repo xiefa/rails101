a0e86e2cecc8bef61749b3340c8a46c9ca535544
xf <xiefahit@gmail.com>
2017-01-01 21:12:24 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  
     before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
