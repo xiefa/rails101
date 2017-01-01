948052767070294bce48fa005b54dad000ddb8fd
xf <xiefahit@gmail.com>
2017-01-01 21:56:58 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  
+
     before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
@@ -11,7 +11,8 @@ class GroupsController < ApplicationController
   end
 
   def show
-     @group = Group.find(params[:id])
+    @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
